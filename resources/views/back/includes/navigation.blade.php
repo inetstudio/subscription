5cb5b0f66d9e05bcbdd6142a309b5906faa2d4eb
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-09 19:58:45 +0300

    Fix icons

--- resources/views/back/includes/navigation.blade.php
+++ resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ isActiveRoute('back.subscription.*') }}">
-    <a href="{{ route('back.subscription.index') }}"><i class="fa fa-newspaper-o"></i> <span class="nav-label">Подписчики</span></a>
+    <a href="{{ route('back.subscription.index') }}"><i class="fa fa-newspaper"></i> <span class="nav-label">Подписчики</span></a>
 </li>
